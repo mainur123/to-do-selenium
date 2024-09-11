64d9edf874ecec549eb7e809faab242c9a614aaf
mainur05 <mainur.rahman@orbitax.com>
2024-09-11 08:15:13 +0600

    Adding github action configurations

--- src/test/java/com/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/qacart/todo/factory/DriverFactory.java
@@ -2,12 +2,15 @@ package com.qacart.todo.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
 public class DriverFactory {
     public WebDriver initializeDriver() {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         return driver;
